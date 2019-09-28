0cbd6dcd6571eed868f0ba91199016eb2c4f449e
t.takahashi <takahashi.developer@gmail.com>
2019-09-29 01:47:31 +0900

    chapter 11

--- src/Money.ts
+++ src/Money.ts
@@ -22,27 +22,11 @@ export default class Money {
   }
 
   static dollar(amount: number): Money {
-    return new Dollar(amount, 'USD');
+    return new Money(amount, 'USD');
   }
 
   static franc(amount: number): Money {
-    return new Franc(amount, 'CHF');
-  }
-
-}
-
-export class Dollar extends Money {
-
-  constructor(amount: number, currency: string) {
-    super(amount, currency);
-  }
-
-}
-
-export class Franc extends Money {
-
-  constructor(amount: number, currency: string) {
-    super(amount, currency);
+    return new Money(amount, 'CHF');
   }
 
 }
--- tests/Money.test.ts
+++ tests/Money.test.ts
@@ -11,24 +11,12 @@ describe('tdd by jest', () => {
   it('test equality', () => {
     expect(Money.dollar(5).equals(Money.dollar(5))).toBe(true);
     expect(Money.dollar(5).equals(Money.dollar(6))).toBe(false);
-    expect(Money.franc(5).equals(Money.franc(5))).toBe(true);
-    expect(Money.franc(5).equals(Money.franc(6))).toBe(false);
     expect(Money.franc(5).equals(Money.dollar(5))).toBe(false);
-  });
-
-  it('test franc multiplication', () => {
-    const five: Money = Money.franc(5);
-    expect(five.times(2)).toEqual(Money.franc(10));
-    expect(five.times(3)).toEqual(Money.franc(15));
   });
 
   it('test currency', () => {
     expect(Money.dollar(1).currency()).toBe('USD');
     expect(Money.franc(1).currency()).toBe('CHF');
-  });
-
-  it('test different class equality', () => {
-    expect(new Money(10, 'CHF').equals(new Franc(10, 'CHF'))).toBe(true);
   });
 
 });
